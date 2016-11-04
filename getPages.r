cb3723a2ad9551ae991afb8c00da928cffd04148
dannyPechersky <danny.pechersky@gmail.com>
2016-11-04 18:25:47 -0400

    removed getPages

--- getPages.r
+++
@@ -1,7 +0,0 @@
-getPages <- function(npages) {
-  data <- vector(mode = "list", length=npages)
-  for (i in 1:npages) {
-    data[[i]] <- citationsQuery("at", page=i, num_results=100)
-  }
-  return(data)
-}
