d667251eeba21c70871ac650ef9ce238e818b6d7
dannyPechersky <danny.pechersky@gmail.com>
2016-11-04 19:56:26 -0400

    improved code

--- queryAltmetric.r
+++ queryAltmetric.r
@@ -8,8 +8,11 @@ queryString <- function(type, required_param, ...) {
   return(paste0(paste(getAltmetricURL(), type, required_param, sep="/"), '?', paste(c(...), collapse='&')))
 }
 
-query <- function(...) {
-  return(fromJSON(file=queryString(...)))
+query <- function(..., include_total=FALSE) {
+  result = fromJSON(file=queryString(...))
+  if (include_total)
+    return(list(results=result$results, total=result$query$total))
+  return(result$results)
 }
 
 getTimeFrames <- function() {
@@ -20,51 +23,20 @@ getTimeFrames <- function() {
            '1y'))
 }
 
-citationsQuery <- function(timeframe, page=NULL, num_results=NULL, cited_in=NULL, doi_prefix=NULL) {
+citationsQuery <- function(timeframe, page=NULL, num_results=NULL, cited_in=NULL, doi_prefix=NULL, include_total=FALSE) {
   params <- c(if(!is.null(page)) paste0("page=", page) else NULL,
               if(!is.null(num_results)) paste0("num_results=", num_results) else NULL,
-              if(!is.null(cited_in)) ? paste0("cited_in=", cited_in) else NULL,
-              if(!is.null(doi_prefix)) ? paste0("doi_prefix=", doi_prefix) else NULL)
+              if(!is.null(cited_in)) paste0("cited_in=", cited_in) else NULL,
+              if(!is.null(doi_prefix)) paste0("doi_prefix=", doi_prefix) else NULL)
   params <- params[!is.null(params)]
-  return(query('citations', timeframe, params))
+  return(query('citations', timeframe, params, include_total))
 }
 
 mergeQueries <- function(queries) {
-  totals <- vector(length = length(queries))
-  pages <- vector(length = length(queries))
-  num_results <- vector(length = length(queries))
-
-  for (i in 1:length(queries)) {
-    query <- queries[[i]]
-    info <- query$query
-    result <- query$result
-    totals[[i]] <- info$total
-    pages[[i]] <- info$page
-    num_results[[i]] <- info$num_results
-  }
-
-  results <- vector(length=sum(num_results), mode = "list")
-
-  offset <- 0
-  for (query in queries) {
-    for (o in 1:query$query$num_results)
-      results[[o+offset]] <- query$results[[o]]
-    offset <- offset + query$query$num_results
-  }
-
-
-  return(list(
-    query=list(
-      total=totals,
-      page=pages,
-      num_results=num_results),
-    results=results))
+  return(unlist(queries, recursive=FALSE))
 }
 
 elementsFromArticles <- function(articles, ...) {
-  results<-vector(mode="list", length=length(articles))
-  elements<-c(...)
-  for (i in 1:length(articles))
-    results[[i]]<-articles[[i]][elements]
-  return(results)
+  elements <- c(...)
+  return(sapply(articles, FUN=function(article) article[elements]))
 }
