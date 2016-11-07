e269782fea714a1f9fa2dd8cf9d6a0f875e3ed59
dannyPechersky <danny.pechersky@gmail.com>
2016-11-07 13:26:38 -0500

    added support for fetch and key

--- queryAltmetric.r
+++ queryAltmetric.r
@@ -16,32 +16,49 @@ query <- function(...) {
   return(fromJSON(file=queryString(...)))
 }
 
-queryID <- function(altmetric_ID) {
-  return(query('id', altmetric_ID))
+queryFetchType <- function(type, id, key, ...) {
+  return(query(paste0(if(fetch) 'fetch/' else '', type), id, c(..., paste0('key=',key))))
 }
 
-queryDOI <- function(doi) {
-  return(query('doi', doi))
+queryIDType <- function(type, id, key=NULL, fetch=FALSE, ...) {
+  if (fetch) {
+    if (is.null(key))
+      stop("Key is necessary for fetch type query")
+    return(queryFetchType(type, id, key, ...))
+  }
+  if (!is.null(key))
+    key_param <- paste0('key=',key)
+  else
+    key_param <- NULL
+  return(query(type, id, key_param))
 }
 
-queryPMID <- function(pmid) {
-  return(query('pmid', pmid))
+queryID <- function(altmetric_ID, key=NULL, fetch=FALSE, ...) {
+  return(queryIDType('id', altmetric_ID, key, fetch, ...))
 }
 
-queryArXiv <- function(arXiv_ID) {
-  return(query('arxiv', arXiv_ID))
+queryDOI <- function(doi, key=NULL, fetch=FALSE, ...) {
+  return(queryIDType('doi', doi, key, fetch, ...))
 }
 
-queryADS <- function(ADS_bibcode) {
-  return(query('ads', ADS_bibcode))
+queryPMID <- function(pmid, key=NULL, fetch=FALSE, ...) {
+  return(queryIDType('pmid', pmid, key, fetch, ...))
 }
 
-queryURI <- function(uri) {
-  return(query('uri', uri))
+queryArXiv <- function(arXiv_ID, key=NULL, fetch=FALSE, ...) {
+  return(queryIDType('arxiv', arXiv_ID, key, fetch, ...))
 }
 
-queryISBN <- function(isbn) {
-  return(query('isbn', isbn))
+queryADS <- function(ADS_bibcode, key=NULL, fetch=FALSE, ...) {
+  return(queryIDType('ads', ADS_bibcode, key, fetch, ...))
+}
+
+queryURI <- function(uri, key=NULL, fetch=FALSE, ...) {
+  return(queryIDType('uri', uri, key, fetch, ...))
+}
+
+queryISBN <- function(isbn, key=NULL, fetch=FALSE, ...) {
+  return(queryIDType('isbn', isbn, key, fetch, ...))
 }
 
 getTimeFrames <- function() {
@@ -52,12 +69,13 @@ getTimeFrames <- function() {
            '1y'))
 }
 
-queryCitations <- function(timeframe, page=NULL, num_results=NULL, cited_in=NULL, doi_prefix=NULL, include_total=FALSE) {
+queryCitations <- function(timeframe, page=NULL, num_results=NULL, cited_in=NULL, doi_prefix=NULL, key=NULL, include_total=FALSE) {
   params <- c(
     if(!is.null(page)) paste0("page=", page) else NULL,
     if(!is.null(num_results)) paste0("num_results=", num_results) else NULL,
     if(!is.null(cited_in)) paste0("cited_in=", cited_in) else NULL,
-    if(!is.null(doi_prefix)) paste0("doi_prefix=", doi_prefix) else NULL)
+    if(!is.null(doi_prefix)) paste0("doi_prefix=", doi_prefix) else NULL,
+    if(!is.null(key)) paste0("key=", key) else NULL)
   params <- params[!is.null(params)]
   result <- query('citations', timeframe, params, include_total)
   if (include_total)
