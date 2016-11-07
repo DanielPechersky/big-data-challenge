0558f89e4c5ac6f72b9569585a4e0cfd348cd0db
dannyPechersky <danny.pechersky@gmail.com>
2016-11-07 12:43:07 -0500

    added wrappers for all API access methods except fetch

--- queryAltmetric.r
+++ queryAltmetric.r
@@ -8,11 +8,36 @@ queryString <- function(type, required_param, ...) {
   return(paste0(paste(getAltmetricURL(), type, required_param, sep="/"), '?', paste(c(...), collapse='&')))
 }
 
-query <- function(..., include_total=FALSE) {
-  result = fromJSON(file=queryString(...))
-  if (include_total)
-    return(list(results=result$results, total=result$query$total))
-  return(result$results)
+query <- function(...) {
+  return(fromJSON(file=queryString(...)))
+}
+
+queryID <- function(altmetric_ID) {
+  return(query('id', altmetric_ID))
+}
+
+queryDOI <- function(doi) {
+  return(query('doi', doi))
+}
+
+queryPMID <- function(pmid) {
+  return(query('pmid', pmid))
+}
+
+queryArXiv <- function(arXiv_ID) {
+  return(query('arxiv', arXiv_ID))
+}
+
+queryADS <- function(ADS_bibcode) {
+  return(query('ads', ADS_bibcode))
+}
+
+queryURI <- function(uri) {
+  return(query('uri', uri))
+}
+
+queryISBN <- function(isbn) {
+  return(query('isbn', isbn))
 }
 
 getTimeFrames <- function() {
@@ -23,14 +48,17 @@ getTimeFrames <- function() {
            '1y'))
 }
 
-citationsQuery <- function(timeframe, page=NULL, num_results=NULL, cited_in=NULL, doi_prefix=NULL, include_total=FALSE) {
+queryCitations <- function(timeframe, page=NULL, num_results=NULL, cited_in=NULL, doi_prefix=NULL, include_total=FALSE) {
   params <- c(
     if(!is.null(page)) paste0("page=", page) else NULL,
     if(!is.null(num_results)) paste0("num_results=", num_results) else NULL,
     if(!is.null(cited_in)) paste0("cited_in=", cited_in) else NULL,
     if(!is.null(doi_prefix)) paste0("doi_prefix=", doi_prefix) else NULL)
   params <- params[!is.null(params)]
-  return(query('citations', timeframe, params, include_total))
+  result <- query('citations', timeframe, params, include_total)
+  if (include_total)
+    return(list(results = result$results, total = result$query$total))
+  return(result$results)
 }
 
 mergeQueries <- function(queries) {
