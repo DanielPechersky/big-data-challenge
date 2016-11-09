c6e9313b9acbd87b9ebfd52be082ebde2f2f21ae
dannyPechersky <danny.pechersky@gmail.com>
2016-11-08 19:19:26 -0500

    queryCitations now works for multiple pages

--- queryAltmetric.r
+++ queryAltmetric.r
@@ -66,20 +66,36 @@ getTimeFrames <- function() {
            '1y'))
 }
 
-queryCitations <- function(timeframe, page=NULL, num_results=NULL, cited_in=NULL, doi_prefix=NULL, key=NULL, include_total=FALSE) {
-  result <- query('citations', timeframe,
-    if(!is.null(page)) paste0("page=", page) else NULL,
+mergeQueries <- function(queries) {
+  return(unlist(queries, recursive=FALSE))
+}
+
+queryCitations <- function(timeframe, page=NULL, num_results=NULL, cited_in=NULL, doi_prefix=NULL, key=NULL, merge_queries=TRUE, include_total=FALSE) {
+  params <- c('citations', timeframe,
     if(!is.null(num_results)) paste0("num_results=", num_results) else NULL,
     if(!is.null(cited_in)) paste0("cited_in=", cited_in) else NULL,
     if(!is.null(doi_prefix)) paste0("doi_prefix=", doi_prefix) else NULL,
     if(!is.null(key)) paste0("key=", key) else NULL)
-  if (include_total)
-    return(list(results = result$results, total = result$query$total))
-  return(result$results)
-}
 
-mergeQueries <- function(queries) {
-  return(unlist(queries, recursive=FALSE))
+  if (is.null(page) || length(page) == 1) {
+    c(params, if(!is.null(page)) paste0("page=", page) else NULL)
+    q <- do.call(query, as.list(params))
+    results <- q$results
+  } else {
+    results = vector(mode = "list", length = length(page))
+    i <- 1
+    for (p in page) {
+      q <- do.call(query, as.list(c(params, paste0("page=", p))))
+      results[[i]] <- q$results
+      i <- i+1
+    }
+    if (merge_queries)
+      results <- mergeQueries(results)
+  }
+
+  if (include_total)
+    return(list(results=results, total=q$query$total))
+  return(results)
 }
 
 elementsFromArticles <- function(articles, ...) {
