5ee66b072dcbc15f27d576fc16a88f01a719a015
dannyPechersky <danny.pechersky@gmail.com>
2016-11-21 13:40:26 -0500

    added readJSONFiles

---
+++ readJSONFiles.r
@@ -0,0 +1,7 @@
+library("rjson")
+
+readJSONFiles <- function(path_to_files, debug=FALSE)
+  lapply(dir(path_to_files, full.names=TRUE, recursive=TRUE),
+         if(debug) function(path) {print(path); return(fromJSON(file=path))}
+         else function(path) fromJSON(file=path))
+
