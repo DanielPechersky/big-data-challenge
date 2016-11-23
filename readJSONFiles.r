8c00e29a3ff083c05450a3ae9d88b358dee6f33c
dannyPechersky <danny.pechersky@gmail.com>
2016-11-23 14:05:35 -0500

    readJSONFiles() now can extract values

--- readJSONFiles.r
+++ readJSONFiles.r
@@ -1,9 +1,16 @@
 library(rjson)
 
-readJSONFiles <- function(path_to_files, debug=FALSE)
-  lapply(dir(path_to_files, full.names=TRUE, recursive=TRUE), 
-         if(debug) function(path) {print(path); return(fromJSON(file=path))}
-         else function(path) fromJSON(file=path))
-
+readJSONFiles <- function(path_to_files, ..., debug=FALSE) {
+  attributes <- c(...)
+  
+  readPath <- 
+    if (length(attributes) == 0) function(path) fromJSON(file=path) 
+  else function(path) fromJSON(file=path)[attributes]
+  
+  return(lapply(dir(path_to_files, pattern="\\.json$", full.names=TRUE, recursive=TRUE),
+                if (debug) function(path) {print(path); return(readPath(path))}
+                else readPath))
+}
+  
 elementsFromArticles <- function(articles, ...)
   sapply(articles, '[', c(...))
